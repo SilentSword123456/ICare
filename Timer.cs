4c13f55c82eaeb9f1aeabcf401e39077635f3d37
SilentSword <andrei925.dumitru@gmail.com>
2026-06-07 09:23:03 +0300

    Implemented the timer finctionality

---
+++ Timer.cs
@@ -0,0 +1,32 @@
+﻿namespace ICare;
+
+public class Timer
+{
+    private readonly Config _config;
+    public bool SkipNext { get; set; } = false;
+    public DateTime BreakDate { get; private set; }
+
+    public Timer(Config config) {
+        _config = config;
+    }
+
+    public async Task Start(CancellationToken token) {
+        while (!token.IsCancellationRequested) {
+            BreakDate = DateTime.Now.AddSeconds(_config.WorkSec);
+            SkipNext = false;
+            try {
+                await Task.Delay(_config.WorkSec * 1000 - 60 * 1000, token);
+                //TODO add notification call send
+                await Task.Delay(60 * 1000, token);
+                if (!SkipNext)
+                    ; //TODO trigger blackscreen
+            }
+            catch (TaskCanceledException) {
+                break;
+            }
+        }
+        
+        Console.WriteLine("Timer stopped");
+        
+    }
+}
