d7e7914033c0c1d5b4564ae1af470e2c5f51a903
SilentSword <andrei925.dumitru@gmail.com>
2026-07-15 21:25:54 +0300

    TEMPORARY CHANGE: Changed the color of the timer arc to Orange

--- Views/OverviewView.xaml.cs
+++ Views/OverviewView.xaml.cs
@@ -37,7 +37,7 @@ public partial class OverviewView : UserControl {
         else {
             SkipStatusLabel.Text = "Scheduled";
             SkipStatusLabel.Foreground = new SolidColorBrush(Colors.DimGray);
-            TimerArc.Stroke = new SolidColorBrush(Colors.DarkGreen);
+            TimerArc.Stroke = new SolidColorBrush(Colors.Orange);
         }
 
         WorkLabel.Text = $"{config.WorkSec / 60}";
